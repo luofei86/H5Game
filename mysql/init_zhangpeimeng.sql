b02210a18d67065e1d2679c4f364790c0136b4b7
luofei <luofei@appchina.com>
2016-08-08 08:55:19 +0800

    Init zhangpeimeng

---
+++ mysql/init_zhangpeimeng.sql
@@ -0,0 +1,53 @@
+INSERT INTO `game_active_info` 
+values(2, '张培萌', 'http://we.yiketalks.com/images/zhangpeimeng/1.jpg', 
+'张培萌', 'zhangpeimeng', 'http://we.yiketalks.com/game/wellcome/callback/zhangpeimeng', 
+'“飞人”张培萌酷炫礼品大发送','', '', '', '2016-08-10', 0, now(), now());
+
+INSERT INTO `game_active_prize_info` values(4, 2, 1, '一等奖', '红米手机+冠军签名明信片一套', 1, 0, now(), now());
+INSERT INTO `game_active_prize_info` values(5, 2, 2, '二等奖', '小米手环+签名照', 1, 0, now(), now());
+INSERT INTO `game_active_prize_info` values(6, 2, 3, '三等奖', '签名明信片一套', 1, 0, now(), now());
+
+
+INSERT INTO `game_question_info` values(6, 2, '最爱物品是', 'http://we.yiketalks.com/images/zhangpeimeng/2.jpg', 0, '21,22,23,24', '21', '成绩', 0, now(), now());
+
+INSERT INTO `game_answer_info`  values(21,'世锦赛4x100米银牌','http://we.yiketalks.com/images/zhangpeimeng_answer/1.png',0,0,now(),now());
+INSERT INTO `game_answer_info`  values(22,'国家队队服','http://we.yiketalks.com/images/zhangpeimeng_answer/2.png',0,0,now(),now());
+INSERT INTO `game_answer_info`  values(23,'手表','http://we.yiketalks.com/images/zhangpeimeng_answer/3.png',0,0,now(),now());
+INSERT INTO `game_answer_info`  values(24,'签名版篮球','http://we.yiketalks.com/images/zhangpeimeng_answer/4.png',0,0,now(),now());
+
+INSERT INTO `game_question_info` values(7, 2, '最爱物品是', 'http://we.yiketalks.com/images/zhangpeimeng/3.jpg', 0, '25,26,27,28', '27', '速度', 0, now(), now());
+
+
+
+INSERT INTO `game_answer_info`  values(25,'国安队签名足球','http://we.yiketalks.com/images/zhangpeimeng_answer/5.png',0,0,now(),now());
+INSERT INTO `game_answer_info`  values(26,'拍立得相机','http://we.yiketalks.com/images/zhangpeimeng_answer/6.png',0,0,now(),now());
+INSERT INTO `game_answer_info`  values(27,'跑鞋','http://we.yiketalks.com/images/zhangpeimeng_answer/7.png',0,0,now(),now());
+INSERT INTO `game_answer_info`  values(28,'各国徽章','http://we.yiketalks.com/images/zhangpeimeng_answer/8.png',0,0,now(),now());
+
+
+INSERT INTO `game_question_info` values(8, 2, '最爱物品是', 'http://we.yiketalks.com/images/zhangpeimeng/4.jpg', 0, '29,30,31,32', '29', '高空', 0, now(), now());
+
+
+INSERT INTO `game_answer_info`  values(29,'航拍器','http://we.yiketalks.com/images/zhangpeimeng_answer/9.png',0,0,now(),now());
+INSERT INTO `game_answer_info`  values(30,'篮球','http://we.yiketalks.com/images/zhangpeimeng_answer/10.png',0,0,now(),now());
+INSERT INTO `game_answer_info`  values(31,'茶壶茶杯','http://we.yiketalks.com/images/zhangpeimeng_answer/11.png',0,0,now(),now());
+INSERT INTO `game_answer_info`  values(32,'粉丝送的画册','http://we.yiketalks.com/images/zhangpeimeng_answer/12.png',0,0,now(),now());
+
+
+INSERT INTO `game_question_info` values(9, 2, '最爱物品是', 'http://we.yiketalks.com/images/zhangpeimeng/5.jpg', 0, '33,34,35,36', '33', '模型', 0, now(), now());
+
+
+INSERT INTO `game_answer_info`  values(33,'车模','http://we.yiketalks.com/images/zhangpeimeng_answer/13.png',0,0,now(),now());
+INSERT INTO `game_answer_info`  values(34,'乐高玩具','http://we.yiketalks.com/images/zhangpeimeng_answer/14.png',0,0,now(),now());
+INSERT INTO `game_answer_info`  values(35,'象棋','http://we.yiketalks.com/images/zhangpeimeng_answer/15.png',0,0,now(),now());
+INSERT INTO `game_answer_info`  values(36,'跳棋','http://we.yiketalks.com/images/zhangpeimeng_answer/16.png',0,0,now(),now());
+
+
+
+INSERT INTO `game_question_info` values(10, 2, '最爱物品是', 'http://we.yiketalks.com/images/zhangpeimeng/1.jpg', 0, '37,38,39,40', '37', '张培萌的狗狗', 0, now(), now());
+
+
+INSERT INTO `game_answer_info`  values(37,'泰迪','http://we.yiketalks.com/images/zhangpeimeng_answer/17.png',0,0,now(),now());
+INSERT INTO `game_answer_info`  values(38,'贵宾犬','http://we.yiketalks.com/images/zhangpeimeng_answer/18.png',0,0,now(),now());
+INSERT INTO `game_answer_info`  values(39,'萨摩耶','http://we.yiketalks.com/images/zhangpeimeng_answer/19.png',0,0,now(),now());
+INSERT INTO `game_answer_info`  values(40,'藏獒','http://we.yiketalks.com/images/zhangpeimeng_answer/20.png',0,0,now(),now());
