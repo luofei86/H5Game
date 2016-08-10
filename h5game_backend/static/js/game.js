dde9051f9b455d8082c2de7c1f05a688b84556f9
luofei <luofei@appchina.com>
2016-08-10 20:50:26 +0800

    fix when scroll the screen the touch start event occur

--- h5game_backend/static/js/game.js
+++ h5game_backend/static/js/game.js
@@ -1,3 +1,5 @@
+var moveys = [];
+
 function init_answer(answer_id){
 	$("#input_answer").val(answer_id);
 	$("#next_play").prop('disabled', false);
@@ -6,83 +8,151 @@ function init_answer(answer_id){
 	$("#img_next_play_enabled").show();
 }
 
-$("#answer_sub_1").on('touchstart', function(){
-	$("#next_play").addClass("btn-disabled");
-	$("#answer_sub_1").animate({
-		opacity:1
-	  }, 1000, function() {
-	  	var answer_value = $("#answer_sub_1").attr("answer-value");
-	  	init_answer(answer_value);
-	  });
-	$("#answer_sub_2").animate({
-		opacity:0.3
-	  }, 1000, function() {
-	    // Animation complete.
-	  });
-	$("#answer_sub_3").animate({
-		opacity:0.3
-	  }, 1000, function() {
-	    // Animation complete.
-	  });
-	$("#answer_sub_4").animate({
-		opacity:0.3
-	  }, 1000, function() {
-	    // Animation complete.
-	  });
+$('#answer_sub_1').on('touchmove', function(e){
+	  var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+	  var elm = $(this).offset();
+	  var y = touch.pageY - elm.top;
+	  moveys.push(y);
 });
 
-$("#answer_sub_2").on('touchstart', function(){
-	$("#next_play").addClass("btn-disabled");
-	$("#answer_sub_1").animate({
-		opacity:0.3
-	  }, 1000, function() {
-	    // Animation complete.
-	  });
-	$("#answer_sub_2").animate({
-		opacity: 1
-	  }, 1000, function() {
-	  	var answer_value = $("#answer_sub_2").attr("answer-value");
-	  	init_answer(answer_value);
-	  });
-	$("#answer_sub_3").animate({
-		opacity: 0.3
-	  }, 1000, function() {
-	    // Animation complete.
-	  });
-	$("#answer_sub_4").animate({
-		opacity: 0.3
-	  }, 1000, function() {
-	    // Animation complete.
-	  });
+$('#answer_sub_1').on('touchstart', function(e){
+	var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+	  var elm = $(this).offset();
+	  var y = touch.pageY - elm.top;
+	  moveys.push(y);
 });
 
-$("#answer_sub_3").on('touchstart', function(){
-	$("#next_play").addClass("btn-disabled");
-	$("#answer_sub_1").animate({
-		opacity:0.3
-	  }, 1000, function() {
-	    // Animation complete.
-	  });
-	$("#answer_sub_2").animate({
-		opacity:0.3
-	  }, 1000, function() {
-	    // Animation complete.
-	  });
-	$("#answer_sub_3").animate({
-		opacity: 1
-	  }, 1000, function() {
-	  	var answer_value = $("#answer_sub_3").attr("answer-value");
-	  	init_answer(answer_value);
-	  });
-	$("#answer_sub_4").animate({
-		opacity:0.3
-	  }, 1000, function() {
-	    // Animation complete.
-	  });
+$('#answer_sub_1').on('touchend', function(e){
+	if(moveys.length == 1 || Math.abs(moveys[moveys.length-1] - moveys[0])<10){
+		$("#next_play").addClass("btn-disabled");
+		$("#answer_sub_1").animate({
+			opacity:1
+		}, 1000, function() {
+			var answer_value = $("#answer_sub_1").attr("answer-value");
+			init_answer(answer_value);
+		});
+		$("#answer_sub_2").animate({
+			opacity:0.3
+		}, 1000, function() {
+		// Animation complete.
+		});
+		$("#answer_sub_3").animate({
+			opacity:0.3
+		}, 1000, function() {
+		// Animation complete.
+		});
+		$("#answer_sub_4").animate({
+			opacity:0.3
+		}, 1000, function() {
+		// Animation complete.
+		});
+	}
+	moveys = [];
 });
 
-$("#answer_sub_4").on('touchstart', function(){
-	$("#next_play").addClass("btn-disabled");
+$('#answer_sub_2').on('touchmove', function(e){
+	  var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+	  var elm = $(this).offset();
+	  var y = touch.pageY - elm.top;
+	  moveys.push(y);
+});
+
+$('#answer_sub_2').on('touchstart', function(e){
+	var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+	  var elm = $(this).offset();	  
+	  var y = touch.pageY - elm.top;
+	  // console.log("x:"+x);
+	  moveys.push(y);
+});
+
+$('#answer_sub_2').on('touchend', function(e){
+	if(moveys.length == 1 || Math.abs(moveys[moveys.length-1] - moveys[0])<10){
+		$("#next_play").addClass("btn-disabled");
+		$("#answer_sub_1").animate({
+			opacity:0.3
+		  }, 1000, function() {
+		    // Animation complete.
+		  });
+		$("#answer_sub_2").animate({
+			opacity: 1
+		  }, 1000, function() {
+		  	var answer_value = $("#answer_sub_2").attr("answer-value");
+		  	init_answer(answer_value);
+		  });
+		$("#answer_sub_3").animate({
+			opacity: 0.3
+		  }, 1000, function() {
+		    // Animation complete.
+		  });
+		$("#answer_sub_4").animate({
+			opacity: 0.3
+		  }, 1000, function() {
+		    // Animation complete.
+		  });
+	}
+	moveys = [];
+});
+
+$('#answer_sub_3').on('touchmove', function(e){
+	  var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+	  var elm = $(this).offset();
+	  var y = touch.pageY - elm.top;
+	  moveys.push(y);
+});
+
+$('#answer_sub_3').on('touchstart', function(e){
+	var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+	  var elm = $(this).offset();
+	  var y = touch.pageY - elm.top;
+	  moveys.push(y);
+});
+
+$('#answer_sub_3').on('touchend', function(e){
+	if(moveys.length == 1 || Math.abs(moveys[moveys.length-1] - moveys[0])<10){
+		$("#next_play").addClass("btn-disabled");
+		$("#answer_sub_1").animate({
+			opacity:0.3
+		  }, 1000, function() {
+		    // Animation complete.
+		  });
+		$("#answer_sub_2").animate({
+			opacity:0.3
+		  }, 1000, function() {
+		    // Animation complete.
+		  });
+		$("#answer_sub_3").animate({
+			opacity: 1
+		}, 1000, function() {
+			var answer_value = $("#answer_sub_3").attr("answer-value");
+			init_answer(answer_value);
+		});
+		$("#answer_sub_4").animate({
+			opacity:0.3
+		  }, 1000, function() {
+		    // Animation complete.
+		  });
+	}
+	moveys = [];
+});
+
+$('#answer_sub_4').on('touchmove', function(e){
+	  var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+	  var elm = $(this).offset();
+	  var y = touch.pageY - elm.top;
+	  moveys.push(y);
+});
+
+$('#answer_sub_4').on('touchstart', function(e){
+	var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+	  var elm = $(this).offset();
+	  var y = touch.pageY - elm.top;
+	  moveys.push(y);
+});
+
+
+$('#answer_sub_4').on('touchend', function(e){
+	if(moveys.length == 1 || Math.abs(moveys[moveys.length-1] - moveys[0])<10){
+		$("#next_play").addClass("btn-disabled");
 	$("#answer_sub_1").animate({
 		opacity:0.3
 	  }, 1000, function() {
@@ -104,4 +174,6 @@ $("#answer_sub_4").on('touchstart', function(){
 	  	var answer_value = $("#answer_sub_4").attr("answer-value");
 	  	init_answer(answer_value);
 	  });
+	}
+	moveys = [];
 });
